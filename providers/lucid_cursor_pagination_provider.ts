04afa495d65abc65955fb82958d1551dfa3e9f4d
mixxtor <113852548+mixxtor@users.noreply.github.com>
2026-03-01 19:22:25 +0700

    fix: ensure type augmentations are included by importing types_augmentation.js

--- providers/lucid_cursor_pagination_provider.ts
+++ providers/lucid_cursor_pagination_provider.ts
@@ -1,6 +1,9 @@
 import { ApplicationService } from '@adonisjs/core/types'
 import { registerCursorPaginationMacro } from '../src/cursor_paginator/macro.js'
 
+// Side-effect import to ensure type augmentations are included when provider is imported
+import '../src/cursor_paginator/types_augmentation.js'
+
 export default class LucidCursorPaginationProvider {
   constructor(protected app: ApplicationService) {}
 
